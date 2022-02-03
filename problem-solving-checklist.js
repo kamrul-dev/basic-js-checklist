b50b825151e5847c1933493244ff5f58781275f6
Kamrul Hasan <you@example.com>
2022-02-03 17:42:18 +0600

    other some problem checklist

---
+++ problem-solving-checklist.js
@@ -0,0 +1,15 @@
+// 1. conversion . function name feetToInch
+
+// 2. conversion . convert centimeter ot meter . function name: centimeterToMeter.
+
+// 3. calculation . function name pageRequirements . how many page print for multiple books.
+
+        // book1 has 100 pages
+        // book2 has 200 pages
+        // book3 has 300 pages
+
+// 4. function name bestFriend.  write friends array and return largest friend name from array.
+
+// 5. function name onlyPositive. Will stop the loop if the array has any negative number and returns all the positive number before the negative number
+
+//
