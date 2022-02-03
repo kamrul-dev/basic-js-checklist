df2b090596c9991ef5afe5b9171232151d597f93
Kamrul Hasan <you@example.com>
2022-02-03 17:17:33 +0600

    Write 3 variables

---
+++ concept-checklist.js
@@ -0,0 +1,5 @@
+
+// 1. write 3 vriables (number, string, boolean)
+// 2.  2 vairables using (let, const)
+// 3.  Simple Math Operation ( +, -, *, /, %)
+//
---
+++ vriables.js
@@ -0,0 +1,5 @@
+//1. write 3 vriables (number, string, boolean)
+
+var number = 1;
+var string = 'kamrul';
+var isChecked = true;
