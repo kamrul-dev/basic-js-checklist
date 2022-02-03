da622eea00d766ec2c38701d80164532552f6f20
Kamrul Hasan <you@example.com>
2022-02-03 17:29:42 +0600

    Check list 12 problem

--- concept-checklist.js
+++ concept-checklist.js
@@ -1,5 +1,24 @@
 
 // 1. write 3 vriables (number, string, boolean)
+
 // 2.  2 vairables using (let, const)
+
 // 3.  Simple Math Operation ( +, -, *, /, %)
-//
+
+// 4. use of comparison operator
+
+// 5. Two conditions. case-1: fulfill both conditions. case-2: fulill at least one conditon
+
+// 6. if - else
+
+// 7. while loop to display 7 to 19 all numbers. only display numbers including 7 to 19 numbers.
+
+// 8. Declare an array. number of elements. 4th position elements update or change, add or remove elements. check whether specific value exists in the array
+
+// 9. use any for loop display every elements of and array.
+
+// 10. You have and array of number . display only the numbers bigger than 80
+
+// 11. write a function that takes three numbers and return multiplication of three numbers.
+
+// 12. declare an object and change any property of that 
---
+++ simple-math.js
@@ -0,0 +1,4 @@
+
+function mathOperation (number1, number2){
+    
+}
