36eb8ac10d67a78d99388bbadbc27f02177ba9c2
Kamrul Hasan <you@example.com>
2022-02-04 13:21:42 +0600

    math operation and comapare

---
+++ comparison.js
@@ -0,0 +1,32 @@
+// 4. use of comparison operator
+
+let a = 6;
+let b = 3;
+let c = 10;
+let isRaining = true;
+
+/* 
+if(a > b){
+    let sum = a - b;
+    console.log(sum);
+}
+ */
+
+/* if(a > b && b < c ){
+    console.log(' a > b and b < c is true');
+} */
+
+
+/* if((a > b && b < c) && isRaining == true ){
+    console.log(' a > b and b < c is true');
+}
+ */
+
+/* if ( a > b || b > c){
+    console.log('ghorar dim dekhaw');
+} */
+
+if ( a == 6 && b == 3 && c == 10){
+    console.log('true condition');
+}
+
--- simple-math.js
+++ simple-math.js
@@ -1,4 +1,20 @@
+//3.  Simple Math Operation ( +, -, *, /, %)
 
-function mathOperation (number1, number2){
-    
-}
+
+let num1 = 5;
+let num2 = 3;
+
+var sum = num1 + num2;
+console.log(sum);
+
+var dif = num1 - num2;
+console.log(dif);
+
+var mul = num1 * num2;
+console.log(mul);
+
+var div = num1 / num2;
+console.log(div.toFixed(2));
+
+var remainder = num1 % num2;
+console.log(remainder);
