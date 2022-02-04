b395e0a039f6c8f26b77768655b6f54c42409dce
Kamrul Hasan <you@example.com>
2022-02-04 13:26:47 +0600

    && and || conditonal checked

---
+++ condition.js
@@ -0,0 +1,18 @@
+// 5. Two conditions. case-1: fulfill both conditions. case-2: fulill at least one conditon
+
+
+// case - 1
+
+let x = 5;
+let y = 3;
+isRaining = true;
+
+/* if(x == 5 && isRaining){
+    console.log("I wil not go out");
+} */
+
+
+//case -2
+if(x > y || isRaining == false){
+    console.log("I will go out anyhow");
+}
