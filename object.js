2c1fe6530537313637a57506a5f98d646d7e5a65
Kamrul Hasan <you@example.com>
2022-02-05 14:52:30 +0600

    change property from an object

---
+++ object.js
@@ -0,0 +1,12 @@
+// 12. declare an object and change any property of that 
+
+let personInfo = {
+    firstName: "kamrul",
+    lastName: "hasan",
+    age: 25,
+    location: "Dhaka, Bangladesh"
+    
+}
+personInfo.firstName = "jamrul";
+personInfo.age = 26;
+console.log(personInfo);
