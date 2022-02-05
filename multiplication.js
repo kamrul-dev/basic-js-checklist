fad797148dfee51ba32fe3c4fc3d99a00a4b7228
Kamrul Hasan <you@example.com>
2022-02-05 14:34:39 +0600

    multiplication within three numbers

---
+++ multiplication.js
@@ -0,0 +1,7 @@
+// 11. write a function that takes three numbers and return multiplication of three numbers.
+
+function multiplication(num1, num2, num3){
+    let mult = num1 * num2 * num3;
+    return mult;
+}
+console.log(multiplication(2, 2, 2));
